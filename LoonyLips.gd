e611e08237f49a524c678b7986536b933f30ed73
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-08-16 22:54:57 +0100

    Import assets and display text to screen

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -3,4 +3,6 @@ extends Node2D
 func _ready():
 	var prompt = ['Mick', 'pizza', 'full']
 	var story = 'Once upon a time a %s ate a %s and felt very %s'
-	print(story % prompt)
+	# $Blackboard/StoryText.text = story % prompt
+	# get_node("Blackboard/StoryText").text = story % prompt
+	$Blackboard/StoryText.bbcode_text = story % prompt
