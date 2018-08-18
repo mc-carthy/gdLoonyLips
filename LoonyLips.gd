aec390878f477bfa8ad128fa18a29cb9d52af60d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-08-18 21:16:40 +0100

    Fill story with player prompts and show to player on completion

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,18 +1,25 @@
 extends Node2D
 
-func _ready():
-	var prompt = ['Mick', 'pizza', 'full']
-	var story = 'Once upon a time a %s ate a %s and felt very %s'
-	# $Blackboard/StoryText.text = story % prompt
-	# get_node("Blackboard/StoryText").text = story % prompt
-	$Blackboard/StoryText.text = story % prompt
+var playerWords = []
+var prompt = ['a name', 'a thing', 'a feeling', 'another feeling', 'some things']
+var story = 'Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s.'
 
+func _ready():
+	$Blackboard/TextBox.text = ""
+	prompt_player()
 
 func _on_TextBox_text_entered(new_text):
-	$Blackboard/StoryText.text = new_text
+	playerWords.append(new_text)
 	$Blackboard/TextBox.text = ""
+	prompt_player()
 
 
 func _on_SubmitTextButton_pressed():
 	var new_text = $Blackboard/TextBox.get_text()
 	_on_TextBox_text_entered(new_text)
+
+func prompt_player():
+	if playerWords.size() == prompt.size():
+		$Blackboard/StoryText.text = story % playerWords
+	else:
+		$Blackboard/StoryText.text = ("Can I have " + prompt[playerWords.size()] + ", please?")
