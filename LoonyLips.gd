5a250e597f034b7713cdea41563f685572692dc6
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-08-18 21:32:20 +0100

    Add restart function to game

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -5,8 +5,8 @@ var prompt = ['a name', 'a thing', 'a feeling', 'another feeling', 'some things'
 var story = 'Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s.'
 
 func _ready():
+	$Blackboard/StoryText.text = "Welcome to the game. \nWe're going to tell a story and have a great time.\nCan I have " + prompt[playerWords.size()] + ", please?"
 	$Blackboard/TextBox.text = ""
-	prompt_player()
 
 func _on_TextBox_text_entered(new_text):
 	playerWords.append(new_text)
@@ -15,11 +15,25 @@ func _on_TextBox_text_entered(new_text):
 
 
 func _on_SubmitTextButton_pressed():
-	var new_text = $Blackboard/TextBox.get_text()
-	_on_TextBox_text_entered(new_text)
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		var new_text = $Blackboard/TextBox.get_text()
+		_on_TextBox_text_entered(new_text)
 
 func prompt_player():
-	if playerWords.size() == prompt.size():
-		$Blackboard/StoryText.text = story % playerWords
+	if is_story_done():
+		tell_story()
 	else:
 		$Blackboard/StoryText.text = ("Can I have " + prompt[playerWords.size()] + ", please?")
+
+func tell_story():
+	$Blackboard/StoryText.text = story % playerWords
+	end_game()
+
+func end_game():
+	$Blackboard/TextBox.queue_free()
+	$Blackboard/SubmitTextButton/ButtonLabel.text = "Retry"
+
+func is_story_done():
+	return playerWords.size() == prompt.size()
