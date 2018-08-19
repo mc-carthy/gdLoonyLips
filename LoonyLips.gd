35e2c8f3e5c9b293b4e7acd0828a78b6bd45258d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-08-19 10:28:17 +0100

    Refactor code to use a dictionary to store prompts & stories

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 var playerWords = []
-var prompt = ['a name', 'a thing', 'a feeling', 'another feeling', 'some things']
-var story = 'Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s.'
+var current_story = {
+		"prompt":['a name', 'a thing', 'a feeling', 'another feeling', 'some things'],
+		"story":"Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s."
+}
 
 func _ready():
-	$Blackboard/StoryText.text = "Welcome to the game. \nWe're going to tell a story and have a great time.\nCan I have " + prompt[playerWords.size()] + ", please?"
+	$Blackboard/StoryText.text = "Welcome to the game. \nWe're going to tell a story and have a great time.\nCan I have " + current_story.prompt[playerWords.size()] + ", please?"
 	$Blackboard/TextBox.text = ""
 
 func _on_TextBox_text_entered(new_text):
@@ -25,10 +27,10 @@ func prompt_player():
 	if is_story_done():
 		tell_story()
 	else:
-		$Blackboard/StoryText.text = ("Can I have " + prompt[playerWords.size()] + ", please?")
+		$Blackboard/StoryText.text = ("Can I have " + current_story.prompt[playerWords.size()] + ", please?")
 
 func tell_story():
-	$Blackboard/StoryText.text = story % playerWords
+	$Blackboard/StoryText.text = current_story.story % playerWords
 	end_game()
 
 func end_game():
@@ -36,4 +38,4 @@ func end_game():
 	$Blackboard/SubmitTextButton/ButtonLabel.text = "Retry"
 
 func is_story_done():
-	return playerWords.size() == prompt.size()
+	return playerWords.size() == current_story.prompt.size()
